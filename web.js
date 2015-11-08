267efa965034b00939175a40861e64742e7baed3
Rahul Ghose <rahul@flywheel.com>
2015-11-08 17:34:21 +0530

    change port, add login form

--- web.js
+++ web.js
@@ -1,14 +1,26 @@
+// Heroku specific configuration
+var app_port = (process.env.PORT || 5000);
+
+// Main applciation start
 var express = require('express');
 var app = express();
 
+// The folder to server static files from is "./public"
+app.use(express.static('public'));
+
+// Allow ejs to render html files as well
+app.set('views', './views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+
 app.get('/', function (req, res) {
-	  res.send('Hello World!');
+	  res.render('login.html', {} );
 });
 
-var server = app.listen(3000, function () {
-	  var host = server.address().address;
-		  var port = server.address().port;
-
-			  console.log('Example app listening at http://%s:%s', host, port);
+var server = app.listen(app_port, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('Example app listening at http://%s:%s', host, port);
 });
 
